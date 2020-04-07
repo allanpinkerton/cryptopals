89e6c8d68642b8b084580de7bcdd8773b811ad90
Jackie <jackiema.pro@gmail.com>
2020-04-06 22:21:25 -0400

    Set 1 Challenge 2
    XOR

---
+++ src/S1C2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	s1 := "1c0111001f010100061a024b53535009181c"
+	s2 := "686974207468652062756c6c277320657965"
+	arr1, _ := hex.DecodeString(s1)
+	arr2, _ := hex.DecodeString(s2)
+	for i, _ := range arr1 {
+		arr1[i] = arr1[i] ^ arr2[i]
+	}
+	result := hex.EncodeToString(arr1)
+
+	fmt.Println(result)
+}
